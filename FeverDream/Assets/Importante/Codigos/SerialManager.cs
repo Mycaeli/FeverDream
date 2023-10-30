868b7765224530f6dfe99592eaaf2a6652391150
Kayre <walejandropg15@gmail.com>
2023-10-29 21:13:52 -0500

    Modify map 1 and intro

--- FeverDream/Assets/Importante/Codigos/SerialManager.cs
+++ FeverDream/Assets/Importante/Codigos/SerialManager.cs
@@ -5,25 +5,46 @@ using System.IO.Ports;
 
 public class SerialManager : MonoBehaviour
 {
-    public static SerialPort puerto; // Esta propiedad será accesible de forma estática
-    public string COM;
+    public static SerialPort puerto; // This property will be accessible statically
+    private string lastDetectedPort; // Store the last detected port
+    private bool portDetected = false; // Flag to check if port is detected
 
     void Start()
     {
-        // Configurar el puerto serie
-        puerto = new SerialPort(COM, 9600);
+        if (!portDetected)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            if (ports.Length > 0)
+            {
+                // Use the first available COM port if any exist
+                lastDetectedPort = ports[0];
+                portDetected = true;
+            }
+            else
+            {
+                Debug.LogWarning("No COM ports available.");
+                return;
+            }
+        }
+
+        // Configure the serial port
+        puerto = new SerialPort(lastDetectedPort, 9600);
         puerto.ReadTimeout = 1;
 
-        // Abrir el puerto
-        puerto.Open();
+        // Open the port if it's not already open
+        if (!puerto.IsOpen)
+        {
+            puerto.Open();
+        }
     }
 
     void OnApplicationQuit()
     {
-        // Cerrar el puerto serie antes de salir de la aplicación
+        // Close the serial port before quitting the application
         if (puerto != null && puerto.IsOpen)
         {
             puerto.Close();
         }
     }
 }
+
---
+++ FeverDream/Assets/SuburbNeighborhoodHousePack/Scripts/Door_closeth.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door_closeth : MonoBehaviour
+{
+    private float distance;
+    private GameObject player;
+    private bool isMoving;
+    private bool isOpen;
+    private Quaternion startRotation;
+    private Quaternion endRotation;
+    public float rotationSpeed = 90.0f; // Adjust the rotation speed as needed
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        isMoving = false;
+        isOpen = false;
+
+        startRotation = transform.rotation;
+        endRotation = Quaternion.Euler(0, 180, 0); // Rotate -80 degrees around the Y-axis when open
+    }
+
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, player.transform.position);
+
+        if (!isMoving && Input.GetButtonDown("Fire1") && distance < 2)
+        {
+            if (isOpen)
+            {
+                // If the door is open, close it
+                isMoving = true;
+            }
+            else
+            {
+                // If the door is closed, open it
+                isMoving = true;
+            }
+        }
+
+        if (isMoving)
+        {
+            // Smoothly interpolate between the start and end rotations
+            float step = rotationSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, isOpen ? startRotation : endRotation, step);
+
+            // Check if the door has reached the desired rotation
+            if (Quaternion.Angle(transform.rotation, isOpen ? startRotation : endRotation) < 0.01f)
+            {
+                isOpen = !isOpen;
+                isMoving = false;
+            }
+        }
+    }
+}
+
