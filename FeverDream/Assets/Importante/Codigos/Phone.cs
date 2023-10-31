e9edb3a5b2d74fab9410a8a4b82b7873a98a04a6
SanKocho_de_Pollo <84156615+NoName588@users.noreply.github.com>
2023-10-31 14:43:20 -0500

    cambios minimos

--- FeverDream/Assets/Importante/Codigos/Phone.cs
+++ FeverDream/Assets/Importante/Codigos/Phone.cs
@@ -3,32 +3,113 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 public class Phone : MonoBehaviour
 {
-    string Code = "123";
-    string Nr = null;
+    string Code = "3481000";
+    string CodeInc = "44442244";
+    string CodeInc2 = "30537700";
+    string CodeEaster = "4448300";
+    string Nr = "";
     int NrIndex = 0;
     string alpha;
     public Text UiText = null;
+
+    private void Update()
+    {
+        // Captura la entrada del teclado numérico
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            CodeFunction("0");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CodeFunction("1");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            CodeFunction("2");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            CodeFunction("3");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            CodeFunction("4");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            CodeFunction("5");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            CodeFunction("6");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            CodeFunction("7");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            CodeFunction("8");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            CodeFunction("9");
+        }
+
+        // Captura la tecla "Enter"
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Enter();
+        }
+
+        // Captura la tecla "Backspace"
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            Delete();
+        }
+    }
 
     public void CodeFunction(string Numbers)
     {
         NrIndex++;
         Nr = Nr + Numbers;
-        UiText.text = Nr;
-
+        UiText.text = Nr; // Actualiza el texto en el objeto UiText
+        Debug.Log("Número oprimido: " + Numbers); // Muestra el número en la consola
     }
+
     public void Enter()
     {
         if (Nr == Code)
         {
-            Debug.Log("funciona");
+            Debug.Log("Código correcto: " + Nr);
+        }
+        else if (Nr == CodeInc)
+        {
+            Debug.Log("Código Incorrecto 1: " + Nr);
+        }
+        else if (Nr == CodeInc2)
+        {
+            Debug.Log("Código Incorrecto 2: " + Nr);
+        }
+        else if (Nr == CodeEaster)
+        {
+            Debug.Log("Código Sospechoso: " + Nr);
+        }
+        else
+        {
+            Debug.Log("Número no apto: " + Nr);
         }
     }
+
     public void Delete()
     {
         NrIndex++;
-        Nr = null;
-        UiText.text = Nr;
+        Nr = "";
+        UiText.text = Nr; // Limpia el texto en el objeto UiText
     }
 }
+
+
