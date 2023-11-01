98410063f9aabdaf153b55b52116b935d9a6b615
SanKocho_de_Pollo <84156615+NoName588@users.noreply.github.com>
2023-10-31 22:56:08 -0500

    noche 3

--- FeverDream/Assets/Importante/Codigos/SlowMotion.cs
+++ FeverDream/Assets/Importante/Codigos/SlowMotion.cs
@@ -1,9 +1,12 @@
+using System.Configuration;
 using UnityEngine;
 
-public class SlowMotion: MonoBehaviour
+public class SlowMotion : MonoBehaviour
 {
     public float slowdownFactor = 0.5f; // Factor de ralentización (0.5 significa la mitad de velocidad).
     public float slowdownDuration = 2f; // Duración de la ralentización en segundos.
+
+    public GameObject canvas; // El Canvas que deseas activar y desactivar.
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,19 +15,28 @@ public class SlowMotion: MonoBehaviour
             Time.timeScale = slowdownFactor;
             Time.fixedDeltaTime = Time.timeScale * 0.02f; // Ajusta el tiempo fijo para la física.
 
-            // Puedes restaurar el tiempo a su valor normal en la OnTriggerExit o después de un cierto tiempo.
-            // Puedes usar una corrutina o un temporizador para eso.
+            // Activa el Canvas cuando el jugador entra en el trigger.
+            if (canvas != null)
+            {
+                canvas.SetActive(true);
+            }
         }
     }
 
-    // Asegúrate de restaurar el tiempo cuando el jugador salga del trigger.
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Time.timeScale = 1f; // Restaura la velocidad del tiempo a su valor normal.
             Time.fixedDeltaTime = 0.02f; // Restaura el tiempo fijo de la física.
+
+            // Desactiva el Canvas cuando el jugador sale del trigger.
+            if (canvas != null)
+            {
+                canvas.SetActive(false);
+            }
         }
     }
 }
+
 
